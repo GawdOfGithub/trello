9ca9c07b49ad336f1c31d54db78d792193fae9a5
Anurag Bhandari <anuragbhandari77736@gmail.com>
2024-01-19 13:38:13 +0530

    Progress saved

--- src/hooks/use-action.ts
+++ src/hooks/use-action.ts
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ActionState,FieldErrors } from "@/lib/create-safe-action";
 type Action<TInput,TOutput> = (data:TInput)=>
     Promise<ActionState<TInput,TOutput>>;
@@ -20,6 +20,41 @@ const [error, setError] = useState<string | undefined>(undefined);
 const [data, setData] = useState<TOutput | undefined>(undefined);
 const [isLoading,setIsLoading] = useState<boolean>(false)
 
+const execute = useCallback(
+    async(input:TInput)=>{
+        setIsLoading(true)
+        try{
+            const result = await action(input)
+            if(!result){
+                return
+            }
+            if(result.fieldErrors){
+                setFieldErrors(result.fieldErrors)
+            }
+            if(result.error){
+                setError(result.error)
+            }
+            if(result.data){
+                setData(result.data)
+                options.onSuccess?.(result.data)
+            }
+        }
+        finally {
+            setIsLoading(false)
+            options.onComplete?.()
+
+        }
+    },[action,options]
+)
+return {
+    execute,
+    fieldErrors,
+    error,
+    data,
+    isLoading
+}
 
 
 }
+
+//4 hr 15 min 
